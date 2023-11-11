9a1b41465e3d36f4ff55492d355533b1d7757d17
piouy001 <piouy_@naver.com>
2023-11-12 00:17:41 +0900

    feat : svg loader 적용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    turbo: {
+      loaders: {
+        ".svg": ["@svgr/webpack"],
+      },
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
