f3e53b4868da5ec4041ee0a188b8f2ddd2bb905c
piouy001 <piouy_@naver.com>
2023-11-14 14:29:21 +0900

    feat : prisma 세팅

---
+++ app/libs/prismadb.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line vars-on-top, no-var, no-unused-vars
+  var prisma: PrismaClient | undefined;
+}
+
+const client = globalThis.prisma ?? new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+export default client;
